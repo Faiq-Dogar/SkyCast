39a89d93eb59e8ac17264196a91a7033454584b5
Faiq-Dogar <faiqd17@gmail.com>
2025-06-14 17:02:27 +0500

    Adding a poiretOne font  14/June/2025

--- .next/static/chunks/src_app_page_tsx_71f5ee22._.js
+++ .next/static/chunks/src_app_page_tsx_71f5ee22._.js
@@ -1,7 +1,7 @@
 (globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/[root-of-the-server]__f1b2a510._.css"
+  "static/chunks/_0a32d984._.js"
 ],
     source: "dynamic"
 });
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Poiret_One } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -10,6 +11,12 @@ const geistSans = Geist({
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
+});
+
+const poiretOne = Poiret_One({
+  weight: "400",
+  subsets: ["latin"],
+  variable: "--font-poiret",
 });
 
 export const metadata: Metadata = {
@@ -25,7 +32,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${poiretOne.variable} antialiased`}
       >
         {children}
       </body>
